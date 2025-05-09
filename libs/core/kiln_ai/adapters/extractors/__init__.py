d37da93bb1f100edae654d1e136070ad75eb90f6
Leonard Q. Marcq <marcqleonard@gmail.com>
2025-05-10 02:34:36 +0800

    fix: coderabbit review

--- libs/core/kiln_ai/adapters/extractors/__init__.py
+++ libs/core/kiln_ai/adapters/extractors/__init__.py
@@ -1,3 +1,10 @@
+"""
+File extractors for processing different document types.
+
+This package provides a framework for extracting content from files
+using different extraction methods.
+"""
+
 from . import base_extractor, gemini_extractor
 
 __all__ = [
