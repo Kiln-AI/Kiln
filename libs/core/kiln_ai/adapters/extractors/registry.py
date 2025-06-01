f06580bd977e273998ab43e8112a647c57e7715d
Leonard Q. Marcq <marcqleonard@gmail.com>
2025-06-01 22:38:40 +0800

    refactor: lru cache on gemini client in registry

--- libs/core/kiln_ai/adapters/extractors/registry.py
+++ libs/core/kiln_ai/adapters/extractors/registry.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from functools import lru_cache
 
 from google import genai
 
@@ -9,14 +9,18 @@ from kiln_ai.utils.config import Config
 from kiln_ai.utils.exhaustive_error import raise_exhaustive_enum_error
 
 
+@lru_cache(maxsize=1)
+def get_genai_client() -> genai.Client:
+    return genai.Client(api_key=Config.shared().gemini_api_key)
+
+
 def extractor_adapter_from_type(
     extractor_type: ExtractorType,
-) -> Callable[[ExtractorConfig], BaseExtractor] | type[BaseExtractor]:
+    extractor_config: ExtractorConfig,
+) -> BaseExtractor:
     match extractor_type:
         case ExtractorType.GEMINI:
-            # TODO: maybe make client global / singleton in GeminiExtractor instead of here
-            client = genai.Client(api_key=Config.shared().gemini_api_key)
-            return lambda extractor_config: GeminiExtractor(client, extractor_config)
+            return GeminiExtractor(get_genai_client(), extractor_config)
         case _:
             # type checking will catch missing cases
             raise_exhaustive_enum_error(extractor_type)
