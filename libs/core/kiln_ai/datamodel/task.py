5f1160b166c521b4bb02dbae58c550e773ba6ab3
tawnymanticore <purplevespa@icloud.com>
2025-06-10 11:12:00 -0700

    passing in structured output from the UI on the Evals page results in Unknown

--- libs/core/kiln_ai/datamodel/task.py
+++ libs/core/kiln_ai/datamodel/task.py
@@ -145,10 +145,14 @@ class TaskRunConfig(KilnParentedModel):
 
         structured_output_mode = data.get("structured_output_mode", None)
         if structured_output_mode is None and "run_config_properties" in data:
-            # Default to unknown. Adapter will have to guess at runtime.
-            data["run_config_properties"]["structured_output_mode"] = (
-                StructuredOutputMode.unknown
-            )
+            run_config_properties = data["run_config_properties"]
+            if not isinstance(run_config_properties, dict):
+                return data
+            if "structured_output_mode" not in run_config_properties:
+                # Default to unknown. Adapter will have to guess at runtime.
+                data["run_config_properties"]["structured_output_mode"] = (
+                    StructuredOutputMode.unknown
+                )
 
         return data
 
