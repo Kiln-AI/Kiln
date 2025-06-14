09338f9c332c041b7827a10e34f539a7afc0ec84
Leonard Q. Marcq <marcqleonard@gmail.com>
2025-06-14 15:20:59 +0800

    refactor: use same UI as settings section root page

--- app/web_ui/src/routes/(app)/docs/[project_id]/+page.svelte
+++ app/web_ui/src/routes/(app)/docs/[project_id]/+page.svelte
@@ -3,6 +3,22 @@
   import { page } from "$app/stores"
 
   let project_id = $page.params.project_id
+
+  let sections = [
+    {
+      name: "Document Library",
+      description: "Manage, upload and browse documents.",
+      button_text: "Browse Documents",
+      href: `/docs/library/${project_id}`,
+    },
+    {
+      name: "Document Extractors",
+      description:
+        "Create and manage document extractors. An extractor is a particular way of extracting information from your documents.",
+      button_text: "Manage Extractors",
+      href: `/docs/extractors/${project_id}`,
+    },
+  ]
 </script>
 
 <AppPage
@@ -11,10 +27,17 @@
   sub_subtitle_link="#"
   action_buttons={[]}
 >
-  <div class="mt-4 grid grid-cols-1 gap-2">
-    <a href={`/docs/library/${project_id}`} class="link">Documents (List)</a>
-    <a href={`/docs/extractors/${project_id}`} class="link"
-      >Document Extractors (List)</a
-    >
+  <div class="flex flex-col gap-8 max-w-[700px] mt-16">
+    {#each sections as section}
+      <div class="flex flex-col md:flex-row gap-4 md:items-center">
+        <div class="grow">
+          <h3 class="font-medium">{section.name}</h3>
+          <p class="text-sm text-gray-500">{section.description}</p>
+        </div>
+        <a href={section.href} class="btn" style="min-width: 14rem">
+          {section.button_text}
+        </a>
+      </div>
+    {/each}
   </div>
 </AppPage>
