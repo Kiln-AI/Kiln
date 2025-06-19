7ecccd2bf27e2669bb9d0695c588cb98be39fea2
Leonard Q. Marcq <marcqleonard@gmail.com>
2025-06-19 16:32:41 +0800

    refactor: move properties validation into model (instead of API)

--- app/web_ui/src/lib/api_schema.d.ts
+++ app/web_ui/src/lib/api_schema.d.ts
@@ -1460,7 +1460,7 @@ export interface components {
              * Name
              * @description A name for this entity.
              */
-            name: string | null;
+            name?: string | null;
             /**
              * Description
              * @description The description of the extractor config
