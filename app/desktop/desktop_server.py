7ffa422526a9a37a266b2ef01e3f370a1820c32a
scosman <scosman@users.noreply.github.com>
2025-07-15 22:01:58 -0400

    Move to a new method of finding the bundled path, the old one didn't work. Verified this locally

--- app/desktop/desktop_server.py
+++ app/desktop/desktop_server.py
@@ -67,10 +67,18 @@ def make_app():
 
 
 def setup_certs():
-    if getattr(sys, "frozen", False):
+    """
+    Not all systems can find the root CA certs.
+
+    We bundle certifi's cacert.pem file with the desktop app (see build_desktop_app.sh),
+
+    We need to set the SSL_CERT_FILE environment variable to the path to the
+    bundled cacert.pem file.
+    """
+    try:
         # If bundled with PyInstaller, get path to bundled cacert.pem
         bundled_cert = os.path.join(sys._MEIPASS, "certifi", "cacert.pem")  # type: ignore
-    else:
+    except Exception:
         # Running as a script, use certifi's cacert.pem
         bundled_cert = certifi.where()
     os.environ["SSL_CERT_FILE"] = bundled_cert
