2bef6d57d27e704007bdf2c45026d193cef5edf0
Leonard Q. Marcq <marcqleonard@gmail.com>
2025-07-18 14:26:09 +0800

    chore: remove unused imports

--- libs/core/kiln_ai/adapters/model_adapters/test_structured_output.py
+++ libs/core/kiln_ai/adapters/model_adapters/test_structured_output.py
@@ -11,7 +11,6 @@ from kiln_ai.adapters.model_adapters.base_adapter import BaseAdapter, RunOutput,
 from kiln_ai.adapters.ollama_tools import ollama_online
 from kiln_ai.adapters.test_prompt_adaptors import get_all_models_and_providers
 from kiln_ai.datamodel import PromptId
-from kiln_ai.datamodel.datamodel_enums import ModelProviderName
 from kiln_ai.datamodel.task import RunConfig, RunConfigProperties
 from kiln_ai.datamodel.test_json_schema import json_joke_schema, json_triangle_schema
 
