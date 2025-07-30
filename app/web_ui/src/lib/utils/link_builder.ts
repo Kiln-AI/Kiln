c8174036b9aac2edf8cb18be3c71f86125da4acd
aiyakitori <223540761+aiyakitori@users.noreply.github.com>
2025-07-30 15:46:56 -0700

    fix finetune prompt link not handled in URL

--- app/web_ui/src/lib/utils/link_builder.ts
+++ app/web_ui/src/lib/utils/link_builder.ts
@@ -6,6 +6,13 @@ export function prompt_link(
   if (!project_id || !task_id || !prompt_id) {
     return undefined
   }
+  // Special case for fine-tuned prompts
+  if (prompt_id.startsWith("fine_tune_prompt::")) {
+    const trimmed_prompt_id = prompt_id.replace("fine_tune_prompt::", "")
+    const fine_tune_id =
+      trimmed_prompt_id.split("::").pop() || trimmed_prompt_id
+    return `/fine_tune/${project_id}/${task_id}/fine_tune/${encodeURIComponent(fine_tune_id)}`
+  }
   if (!prompt_id.includes("::")) {
     // not an ID style prompt, link to static
     return `/prompts/${project_id}/${task_id}/generator_details/${encodeURIComponent(prompt_id)}`
