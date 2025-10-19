dd6fbfcdadb91df040c74e4665ca858ba75fa2b7
scosman <scosman@users.noreply.github.com>
2025-10-19 10:34:39 -0400

    Register with PH if the user explictly registered for work/commercial usage

--- app/web_ui/src/routes/+layout.ts
+++ app/web_ui/src/routes/+layout.ts
@@ -1,6 +1,7 @@
 import posthog from "posthog-js"
 import { browser } from "$app/environment"
 import { dev } from "$app/environment"
+import { client } from "$lib/api_client"
 
 export const prerender = true
 export const ssr = false
@@ -14,6 +15,35 @@ export const load = async () => {
       capture_pageleave: false,
       autocapture: false,
     })
+    setup_ph_work_user()
   }
   return
+}
+
+async function setup_ph_work_user() {
+  try {
+    const { data, error } = await client.GET("/api/settings")
+    if (error) {
+      throw error
+    }
+
+    // Only identify the user if the user explicitly registered as a work user (for commercial use) during setup
+    // Users who specify they are using Kiln for personal use are not identified
+    if (
+      data.user_type === "work" &&
+      typeof data.work_use_contact === "string"
+    ) {
+      posthog.identify(data.work_use_contact, {
+        email: data.work_use_contact,
+        user_type: data.user_type,
+      })
+    } else {
+      posthog.setPersonProperties({
+        user_type: data.user_type,
+      })
+    }
+  } catch (error) {
+    // Non critical error, just log it
+    console.error("Error setting up PostHog", error)
+  }
 }
