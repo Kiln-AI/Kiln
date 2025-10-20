ca75975f9ea25b1fcb295405a140c702ff837368
scosman <scosman@users.noreply.github.com>
2025-10-20 10:14:59 -0400

    upgrade eslint

---
+++ app/web_ui/eslint.config.mjs
@@ -0,0 +1,92 @@
+import globals from "globals"
+import js from "@eslint/js"
+import tsParser from "@typescript-eslint/parser"
+import typescriptEslint from "@typescript-eslint/eslint-plugin"
+import sveltePlugin from "eslint-plugin-svelte"
+import svelteParser from "svelte-eslint-parser"
+import prettier from "eslint-config-prettier"
+
+export default [
+  {
+    ignores: [
+      "**/.DS_Store",
+      "**/node_modules",
+      "**/build",
+      "**/.svelte-kit",
+      "**/package",
+      "**/.env",
+      "**/.env.*",
+      "!**/.env.example",
+      "**/pnpm-lock.yaml",
+      "**/package-lock.json",
+      "**/yarn.lock",
+    ],
+  },
+  js.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      parser: tsParser,
+      sourceType: "module",
+      ecmaVersion: 2020,
+
+      parserOptions: {
+        extraFileExtensions: [".svelte"],
+      },
+
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+    },
+
+    rules: {
+      ...typescriptEslint.configs.recommended.rules,
+      "no-undef": "off",
+      "no-unused-vars": "off",
+
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_$",
+          varsIgnorePattern: "^_$",
+          caughtErrorsIgnorePattern: "^_$",
+        },
+      ],
+
+      "no-console": [
+        "error",
+        {
+          allow: ["warn", "error", "info"],
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.svelte"],
+
+    plugins: {
+      svelte: sveltePlugin,
+    },
+
+    languageOptions: {
+      parser: svelteParser,
+
+      parserOptions: {
+        parser: {
+          ts: "@typescript-eslint/parser",
+          js: "espree",
+          typescript: "@typescript-eslint/parser",
+        },
+      },
+    },
+
+    rules: {
+      ...sveltePlugin.configs.recommended.rules,
+    },
+  },
+]
