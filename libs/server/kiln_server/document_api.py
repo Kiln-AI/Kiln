c0e0703380f670c0360dcd0f4ac30c18282b6d60
Leonard Q. Marcq <marcqleonard@gmail.com>
2025-10-30 15:43:26 +0800

    fix: typo

--- libs/server/kiln_server/document_api.py
+++ libs/server/kiln_server/document_api.py
@@ -578,7 +578,7 @@ async def build_rag_workflow_runner(
     # refactor this to use better global rate limiting / throttling
     # we currently only throttle within a single job, but here we parallelize jobs
     # which causes too many concurrent subjobs to run (e.g. PDF split up into pages, each page is extracted in parallel)
-    # ollama and local providers get easily stuck depending on hardward
+    # ollama and local providers get easily stuck depending on hardware
     extractor_concurrency = 5
     if extractor_config.model_provider_name == ModelProviderName.ollama:
         extractor_concurrency = 1
