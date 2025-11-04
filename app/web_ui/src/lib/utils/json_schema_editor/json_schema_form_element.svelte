7036fbe388a0b8967e47100b252361eab39e2d02
scosman <scosman@users.noreply.github.com>
2025-11-03 19:56:33 -0500

    Allow creating more complex schemas when you create a task, including enums and nested objects

--- app/web_ui/src/lib/utils/json_schema_editor/json_schema_form_element.svelte
+++ app/web_ui/src/lib/utils/json_schema_editor/json_schema_form_element.svelte
@@ -5,8 +5,8 @@
     schema_from_model,
   } from "$lib/utils/json_schema_editor/json_schema_templates"
   import Dialog from "$lib/ui/dialog.svelte"
+  import JsonSchemaObject from "./json_schema_object.svelte"
 
-  let validation_errors: string[] = []
   let id = Math.random().toString(36)
 
   // Two parallel models in this component
@@ -26,73 +26,7 @@
     }
   }
 
-  async function add_property() {
-    schema_model.properties.push({
-      id: "",
-      title: "",
-      description: "",
-      type: "string",
-      required: true,
-    })
-    // Trigger reactivity
-    schema_model = schema_model
-    // Scroll new item into view. Async to allow rendering first
-    setTimeout(() => {
-      const property = document.getElementById(
-        "property_" + (schema_model.properties.length - 1) + "_" + id,
-      )
-      if (property) {
-        property.scrollIntoView({ block: "center" })
-      }
-    }, 1)
-  }
-
-  function remove_property(index: number) {
-    const property = schema_model.properties[index]
-    const isPropertyEdited = property.title || property.description
-
-    if (
-      !isPropertyEdited ||
-      confirm(
-        "Are you sure you want to remove Property #" +
-          (index + 1) +
-          "?\n\nIt has content which hasn't been saved.",
-      )
-    ) {
-      schema_model.properties.splice(index, 1)
-      // trigger reactivity
-      schema_model = schema_model
-      // Move the page to the top anchor
-      const list = document.getElementById(id)
-      if (list) {
-        // Scroll to the top of the list
-        setTimeout(() => {
-          list.scrollIntoView()
-        }, 1)
-      }
-    }
-  }
-
   let raw_json_schema_dialog: Dialog | null = null
-
-  // We have some types in the drop down we don't actually support.
-  // When selected, we want to show the raw JSON Schema modal to give users a choice.
-  const unsupported_types = ["array", "object", "enum", "other"]
-  function selected_type(e: Event, index: number) {
-    const target = e.target as HTMLSelectElement
-    const value = target.value
-    if (!unsupported_types.includes(value)) {
-      // This type is supported, all good
-      return
-    }
-
-    // Block actually changing the type, keep the old value which should be valid
-    const prior_value = schema_model.properties[index].type
-    target.value = prior_value
-
-    // Show the dialog so the user can choose
-    raw_json_schema_dialog?.show()
-  }
 
   function switch_to_raw_schema(): boolean {
     raw = true
@@ -114,97 +48,17 @@
       raw = false
     }
   }
+
+  function show_raw_json_schema_dialog() {
+    raw_json_schema_dialog?.show()
+  }
 </script>
 
 {#if !raw}
-  {#if validation_errors.length > 0}
-    <div class="validation-errors">
-      {#each validation_errors as error}
-        <div class="text-error">{error}</div>
-      {/each}
-    </div>
-  {:else}
-    <div class="flex flex-col gap-8 pt-6" {id}>
-      {#each schema_model.properties as property, index}
-        {#if property}
-          <!-- ignore that we don't use this var-->
-        {/if}
-
-        <div class="flex flex-col gap-2">
-          <div
-            class="flex flex-row gap-3 font-medium text-sm pb-2"
-            id={"property_" + index + "_" + id}
-          >
-            <div class="grow">
-              Property #{index + 1}
-            </div>
-            <button
-              class="link text-xs text-gray-500"
-              on:click={() => remove_property(index)}
-            >
-              remove
-            </button>
-          </div>
-          <div class="flex flex-row gap-3">
-            <div class="grow">
-              <FormElement
-                id={"property_" + property.id + "_title"}
-                label="Property Name"
-                inputType="input"
-                bind:value={schema_model.properties[index].title}
-                light_label={true}
-              />
-            </div>
-            <FormElement
-              id={"property_" + property.id + "_type"}
-              label="Type"
-              inputType="select"
-              bind:value={schema_model.properties[index].type}
-              on_select={(e) => selected_type(e, index)}
-              select_options={[
-                ["string", "String"],
-                ["number", "Number"],
-                ["integer", "Integer"],
-                ["boolean", "Boolean"],
-                ["array", "Array"],
-                ["object", "Object"],
-                ["enum", "Enum"],
-                ["other", "More..."],
-              ]}
-              light_label={true}
-            />
-            <FormElement
-              id={"property_" + property.id + "_required"}
-              label="Required"
-              inputType="select"
-              bind:value={schema_model.properties[index].required}
-              select_options={[
-                [true, "True"],
-                [false, "False"],
-              ]}
-              light_label={true}
-            />
-          </div>
-          <FormElement
-            id={"property_" + property.id + "_description"}
-            label="Description"
-            inputType="input"
-            bind:value={schema_model.properties[index].description}
-            light_label={true}
-          />
-        </div>
-      {/each}
-      <div class="flex place-content-center">
-        <button
-          class="btn btn-sm"
-          on:click={() => add_property()}
-          id={"add_button_" + id}
-        >
-          Add Property
-        </button>
-      </div>
-    </div>
-  {/if}
+  <JsonSchemaObject
+    {schema_model}
+    on:show-raw-json-schema={show_raw_json_schema_dialog}
+  />
 {:else}
   <div class="flex flex-col gap-4 pt-6" {id}>
     <FormElement
---
+++ app/web_ui/src/lib/utils/json_schema_editor/json_schema_item.svelte
@@ -0,0 +1,198 @@
+<script lang="ts">
+  import FormElement from "../form_element.svelte"
+  import {
+    type SchemaModelProperty,
+    type SchemaModelTypedObject,
+  } from "./json_schema_templates"
+  import { createEventDispatcher } from "svelte"
+  import JsonSchemaObject from "./json_schema_object.svelte"
+
+  const dispatch = createEventDispatcher()
+
+  let id = Math.random().toString(36)
+  export let model: SchemaModelProperty = {
+    id: "",
+    title: "",
+    description: "",
+    type: "string",
+    required: true,
+  }
+  let type_option:
+    | "string"
+    | "number"
+    | "integer"
+    | "boolean"
+    | "array"
+    | "object"
+    | "enum" = "string"
+
+  let enum_text: string = ""
+
+  let object_model: SchemaModelTypedObject | undefined = undefined
+  $: object_model =
+    model.type === "object" && model.properties
+      ? (model as SchemaModelTypedObject)
+      : undefined
+
+  // We have some types in the drop down we don't actually support.
+  // When selected, we want to show the raw JSON Schema modal to give users a choice.
+  // TODO reduce this list
+  const unsupported_types = ["array", "other"]
+  function selected_type(e: Event) {
+    const target = e.target as HTMLSelectElement
+    const value = target.value
+
+    if (unsupported_types.includes(value)) {
+      // Block actually changing the type, keep the old value which should be valid
+      const prior_value = model.type
+      target.value = prior_value
+
+      // Show the dialog so the user can choose
+      dispatch("show-raw-json-schema")
+    }
+  }
+
+  function type_changed(
+    new_type:
+      | "string"
+      | "number"
+      | "integer"
+      | "boolean"
+      | "array"
+      | "object"
+      | "enum",
+  ) {
+    // Setup properties for object type (or clear if not supported)
+    if (new_type === "object") {
+      model.properties = model.properties ?? []
+    } else {
+      model.properties = undefined
+    }
+
+    // Setup enum for enum type
+    // enums are special var on string type, not their own type
+    if (new_type === "enum") {
+      model.enum = model.enum ?? []
+      model.type = "string"
+    } else {
+      model.enum = undefined
+      model.type = new_type
+    }
+
+    // Trigger reactivity
+    model = model
+  }
+  $: type_changed(type_option)
+
+  function add_enum_value() {
+    const trimmed_text = enum_text.trim()
+    if (trimmed_text === "") {
+      return
+    }
+
+    if (!model.enum?.includes(trimmed_text)) {
+      model.enum?.push(trimmed_text)
+    }
+    enum_text = ""
+    model = model
+  }
+
+  function remove_enum_value(value: unknown) {
+    model.enum = model.enum?.filter((v) => v !== value)
+    model = model
+  }
+</script>
+
+<div class="flex flex-row gap-3">
+  <div class="grow">
+    <FormElement
+      id={"property_" + id + "_title"}
+      label="Property Name"
+      inputType="input"
+      bind:value={model.title}
+      light_label={true}
+    />
+  </div>
+  <FormElement
+    id={"property_" + id + "_type"}
+    label="Type"
+    inputType="select"
+    bind:value={type_option}
+    on_select={(e) => selected_type(e)}
+    select_options={[
+      ["string", "String"],
+      ["number", "Number"],
+      ["integer", "Integer"],
+      ["boolean", "Boolean"],
+      ["array", "Array"],
+      ["object", "Object"],
+      ["enum", "Enum"],
+      ["other", "More..."],
+    ]}
+    light_label={true}
+  />
+  <FormElement
+    id={"property_" + id + "_required"}
+    label="Required"
+    inputType="select"
+    bind:value={model.required}
+    select_options={[
+      [true, "True"],
+      [false, "False"],
+    ]}
+    light_label={true}
+  />
+</div>
+<FormElement
+  id={"property_" + id + "_description"}
+  label="Description"
+  inputType="input"
+  bind:value={model.description}
+  light_label={true}
+/>
+
+<!-- Per type fields -->
+
+{#if object_model}
+  <div class="ml-4 pl-4 border-l">
+    <div class="font-medium text-sm pt-1">Object Properties</div>
+    <JsonSchemaObject bind:schema_model={object_model} />
+  </div>
+{/if}
+
+{#if type_option === "enum"}
+  <div class="flex flex-row gap-3">
+    <div class="w-64">
+      <FormElement
+        id={"property_" + id + "_enum"}
+        label="Enum Values"
+        inputType="input"
+        optional={true}
+        info_msg=" "
+        placeholder="Enter a value"
+        bind:value={enum_text}
+        light_label={true}
+      />
+    </div>
+    <div class="mt-5 flex flex-row gap-6 grow">
+      <button
+        class="btn btn-primary"
+        on:click={add_enum_value}
+        disabled={enum_text.trim() === ""}>Add</button
+      >
+      <div
+        class="flex flex-row flex-wrap grow gap-1 place-content-center items-center"
+      >
+        {#each model.enum ?? [] as value}
+          <button
+            class="badge badge-outline"
+            on:click={() => remove_enum_value(value)}
+          >
+            {value}
+            <span class="pl-2">&times;</span>
+          </button>
+        {/each}
+      </div>
+    </div>
+  </div>
+{/if}
