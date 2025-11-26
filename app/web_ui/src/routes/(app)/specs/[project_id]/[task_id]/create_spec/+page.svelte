1b9b8a296f3ecf0d8d4a6939f88c67e3cd997f2d
Sam Fierro <13154106+sfierro@users.noreply.github.com>
2025-11-26 12:07:42 -0800

    wip

--- app/web_ui/src/routes/(app)/specs/[project_id]/[task_id]/create_spec/+page.svelte
+++ app/web_ui/src/routes/(app)/specs/[project_id]/[task_id]/create_spec/+page.svelte
@@ -9,6 +9,8 @@
   import { goto } from "$app/navigation"
   import { client } from "$lib/api_client"
   import FormElement from "$lib/utils/form_element.svelte"
+  import Dialog from "$lib/ui/dialog.svelte"
+  import Warning from "$lib/ui/warning.svelte"
 
   // ### Spec Creation Form ###
 
@@ -37,6 +39,12 @@
   let create_error: KilnError | null = null
   let submitting = false
   let complete = false
+
+  let analyze_dialog: Dialog | null = null
+  async function analyze_spec() {
+    analyze_dialog?.show()
+    submitting = false
+  }
 
   async function create_spec() {
     try {
@@ -94,14 +102,13 @@
     ]}
   >
     <FormContainer
-      submit_label="Create Spec"
-      on:submit={create_spec}
+      submit_label="Next"
+      on:submit={analyze_spec}
       bind:error={create_error}
       bind:submitting
       warn_before_unload={!!(!complete && (spec_definition || name))}
     >
       {#if tool_function_name}
-        <!-- TODO: Display tool name somehow -->
         <span class="text-sm text-gray-500"
           >Tool Function Name: {tool_function_name}</span
         >
@@ -126,6 +133,24 @@
           label: "Reset",
         }}
       />
+      <Warning
+        warning_message="Next we'll analyze your spec to ensure it's clear and actionable for an AI judge. You'll have a chance to review and refine it before creating the spec."
+        warning_color="warning"
+        warning_icon="exclaim"
+      />
     </FormContainer>
+    <div class="flex flex-row gap-1 mt-2 justify-end">
+      <span class="text-xs text-gray-500">or</span>
+      <button
+        class="link underline text-xs text-gray-500"
+        on:click={create_spec}>Create Spec Without Analysis</button
+      >
+    </div>
   </AppPage>
 </div>
+
+<Dialog bind:this={analyze_dialog} title="Analyzing Spec">
+  <div class="flex flex-col items-center justify-center min-h-[100px]">
+    <div class="loading loading-spinner loading-lg"></div>
+  </div>
+</Dialog>
