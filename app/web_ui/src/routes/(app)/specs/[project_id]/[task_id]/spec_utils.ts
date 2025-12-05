d558a6f9d0c759a9ec3a6367b61994402cab41c9
Sam Fierro <13154106+sfierro@users.noreply.github.com>
2025-12-04 17:22:56 -0800

    create eval when spec created

--- app/web_ui/src/routes/(app)/specs/[project_id]/[task_id]/spec_utils.ts
+++ app/web_ui/src/routes/(app)/specs/[project_id]/[task_id]/spec_utils.ts
@@ -1,7 +1,14 @@
 import { client } from "$lib/api_client"
 import { goto } from "$app/navigation"
-import type { SpecProperties, SpecType } from "$lib/types"
+import type {
+  EvalDataType,
+  EvalOutputScore,
+  EvalTemplateId,
+  SpecProperties,
+  SpecType,
+} from "$lib/types"
 import { buildDefinitionFromProperties } from "./select_template/spec_templates"
+import { createKilnError } from "$lib/utils/error_handlers"
 
 /**
  * Navigate to review_spec page after storing form data
@@ -50,6 +57,13 @@ export async function createSpec(
   spec_type: SpecType,
   property_values: Record<string, string | null>,
 ): Promise<string | null> {
+  // First create a new eval for the spec under the hood
+
+  const eval_id = await createEval(project_id, task_id, name, spec_type)
+  if (!eval_id) {
+    throw createKilnError("Failed to create eval for spec")
+  }
+
   // Build the properties object with spec_type
   const properties = {
     spec_type: spec_type,
@@ -72,7 +86,7 @@ export async function createSpec(
         priority: 1,
         status: "active",
         tags: [],
-        eval_id: null,
+        eval_id: eval_id,
       },
     },
   )
@@ -88,4 +102,100 @@ export async function createSpec(
   }
 
   return data?.id || null
+}
+
+async function createEval(
+  project_id: string,
+  task_id: string,
+  spec_name: string,
+  spec_type: SpecType,
+): Promise<string | null> {
+  const name = spec_name
+  const description = `An eval to measure if the model's behaviour meets the spec: ${spec_name}.`
+  const template = specEvalTemplate(spec_type)
+  const output_scores = [specEvalOutputScore(spec_type)]
+  const tag = specEvalTag(spec_name)
+  const eval_set_filter_id = `tag::${tag}`
+  const eval_configs_filter_id = `tag::${tag}_golden`
+  const template_properties = {} // TODO: Refactor evals to use spec definition instead?
+  const evaluation_data_type = specEvalDataType(spec_type)
+  const { data, error } = await client.POST(
+    "/api/projects/{project_id}/tasks/{task_id}/create_evaluator",
+    {
+      params: {
+        path: { project_id, task_id },
+      },
+      body: {
+        name,
+        description,
+        template,
+        output_scores,
+        eval_set_filter_id,
+        eval_configs_filter_id,
+        template_properties,
+        evaluation_data_type,
+      },
+    },
+  )
+  if (error) {
+    throw error
+  }
+  return data?.id || null
+}
+
+function specEvalOutputScore(spec_type: SpecType): EvalOutputScore {
+  return {
+    name: spec_type,
+    type: "pass_fail",
+    instruction: "Evaluate if the model's behaviour meets the spec.",
+  }
+}
+
+function specEvalDataType(spec_type: SpecType): EvalDataType {
+  if (spec_type === "appropriate_tool_use") {
+    return "full_trace"
+  }
+  if (spec_type === "reference_answer_accuracy") {
+    return "reference_answer"
+  }
+  return "final_answer"
+}
+
+function specEvalTemplate(spec_type: SpecType): EvalTemplateId | null {
+  if (spec_type === "appropriate_tool_use") {
+    return "tool_call"
+  }
+  if (spec_type === "reference_answer_accuracy") {
+    return "rag"
+  }
+  if (spec_type === "behaviour") {
+    return "kiln_issue"
+  }
+  if (spec_type === "factual_correctness") {
+    return "factual_correctness"
+  }
+  if (spec_type === "toxicity") {
+    return "toxicity"
+  }
+  if (spec_type === "bias") {
+    return "bias"
+  }
+  if (spec_type === "maliciousness") {
+    return "maliciousness"
+  }
+  if (spec_type === "jailbreak") {
+    return "jailbreak"
+  }
+  return null
+}
+
+function specEvalTag(spec_name: string): string {
+  const tag = spec_name.toLowerCase().replace(/ /g, "_")
+  if (tag.length === 0) {
+    return "eval_" + (Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000)
+  }
+  if (tag.length > 32) {
+    return tag.slice(0, 32)
+  }
+  return tag
 }
