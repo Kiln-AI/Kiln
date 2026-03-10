cedbd3a5acdf9b1cfc20670714bd284274022210
Leonard Q. Marcq <marcqleonard@gmail.com>
2026-03-10 17:26:07 +0800

    feat: chat UI

--- app/web_ui/src/routes/(app)/+layout.svelte
+++ app/web_ui/src/routes/(app)/+layout.svelte
@@ -30,6 +30,7 @@
     Specs,
     Generate,
     Run,
+    Chat,
     FineTune,
     Models,
     Optimize,
@@ -67,6 +68,8 @@
       section = Section.Specs
     } else if (path_start("/optimize", $page.url.pathname)) {
       section = Section.Optimize
+    } else if (path_start("/chat", $page.url.pathname)) {
+      section = Section.Chat
     } else {
       section = Section.None
     }
@@ -172,6 +175,25 @@
             />
           </svg>
           Run</a
+        >
+      </li>
+      <li class="menu-md">
+        <a href="/chat" class={section == Section.Chat ? "active" : ""}>
+          <svg
+            class="w-6 h-6 mr-2"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M8 12H8.01M12 12H12.01M16 12H16.01M21 12C21 16.4183 16.9706 20 12 20C10.4607 20 9.01172 19.6565 7.74467 19.0511L3 20L4.39499 16.28C3.51156 15.0424 3 13.5743 3 12C3 7.58172 7.02944 4 12 4C16.9706 4 21 7.58172 21 12Z"
+              stroke="currentColor"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+          Chat</a
         >
       </li>
       <li class="menu-md">
