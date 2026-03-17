bec225c7613cbf43149657bb5e66a0501235a0aa
aiyakitori <16633065+chiang-daniel@users.noreply.github.com>
2026-03-17 15:06:04 -0700

    fix SDG flow

--- app/web_ui/src/routes/(app)/fine_tune/[project_id]/[task_id]/create_finetune/select_finetune_dataset.svelte
+++ app/web_ui/src/routes/(app)/fine_tune/[project_id]/[task_id]/create_finetune/select_finetune_dataset.svelte
@@ -285,7 +285,7 @@
       splits: "fine_tune_data:1.0",
       finetune_link: `/fine_tune/${project_id}/${task_id}/create_finetune`,
     })
-    if (required_tool_ids && required_tool_ids.length > 0) {
+    if (required_tool_ids !== undefined) {
       params.set("fine_tuning_tools", required_tool_ids.join(","))
     }
     let link = `/dataset/${project_id}/${task_id}/add_data?${params.toString()}`
