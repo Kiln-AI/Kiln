73878b7a3c8cf910f63a26e5824cb23f1db26ec2
scosman <scosman@users.noreply.github.com>
2026-04-02 08:46:33 -0400

    WIP chat pane

---
+++ app/web_ui/src/routes/(app)/chat/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import AppPage from "../app_page.svelte"
+  import Chat from "./chat.svelte"
+</script>
+
+<AppPage
+  title="Chat"
+  subtitle="Streaming chat"
+  limit_max_width={true}
+  no_y_padding={true}
+>
+  <Chat />
+</AppPage>
