124047fd23216f92c165c744402565cd972598c5
scosman <scosman@users.noreply.github.com>
2026-04-02 15:07:35 -0400

    remove files from another branch

--- app/web_ui/src/routes/(app)/chat/+page.svelte
+++ app/web_ui/src/routes/(app)/chat/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import AppPage from "../app_page.svelte"
   import Chat from "./chat.svelte"
-  import EmptyChat from "./empty_chat.svelte"
 </script>
 
 <AppPage
@@ -10,6 +9,5 @@
   limit_max_width={true}
   no_y_padding={true}
 >
-  <EmptyChat />
   <Chat />
 </AppPage>
